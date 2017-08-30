71d8dfad77071ed05ed3aafd02d00bbee63946cd
Cristobal Nuñes <cenunez47@gmail.com>
2017-08-30 02:03:04 +0000

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,11 @@
-## code your solution here. 
+## code your solution here.
+class Cat
+  attr_accessor :name
+  def meow
+    puts "meow!"
+  end
+end
+
+garf = Cat.new
+garf.name = "Garfield"
+garf.meow
